0cd41f30f6c3ef69ff52446d6c8ade0f56dd397a
Milan Dierick <milan.dierick@gmail.com>
2019-04-03 14:50:37 +0200

    Fixed RGB calculation

--- ImageDisplayer/ImageView.cs
+++ ImageDisplayer/ImageView.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
@@ -9,53 +11,114 @@ namespace ImageDisplayer
     {
         private const int ImageWidth = 256;
         private const int ImageHeight = 192;
-        
-        private byte[] buffer;
-        private byte[] tmpBuffer;
+        private readonly int _counterY = 0;
+        private readonly int _counterU = 0;
+        private readonly int _counterV = 0;
+        private readonly bool _isFirstCycle = true;
 
-        private byte[] y;
-        private byte[] u;
-        private byte[] v;
-        private byte[] r;
-        private byte[] g;
-        private byte[] b;
+        private readonly byte[] _buffer = File.ReadAllBytes(Environment.CurrentDirectory + "/image.yuv");
 
+        private readonly byte[] _y;
+        private readonly byte[] _u;
+        private readonly byte[] _v;
+        private readonly byte[] _r;
+        private readonly byte[] _g;
+        private readonly byte[] _b;
 
+        private Bitmap bmp;
 
         public ImageView()
         {
             InitializeComponent();
 
-            buffer = File.ReadAllBytes(Environment.CurrentDirectory + "/image.yuv");
-            r = new byte[sizeof(byte) * ImageWidth * ImageHeight * 3];
-            g = new byte[sizeof(byte) * ImageWidth * ImageHeight * 3];
-            b = new byte[sizeof(byte) * ImageWidth * ImageHeight * 3];
+            _r = new byte[ImageWidth * ImageHeight];
+            _g = new byte[ImageWidth * ImageHeight];
+            _b = new byte[ImageWidth * ImageHeight];
 
-            y = buffer.Skip(0).Take(ImageWidth * ImageHeight).ToArray();
-            u = buffer.Skip(ImageWidth * ImageHeight).Take(ImageWidth * ImageHeight / 4).ToArray();
-            v = buffer.Skip(ImageWidth * ImageHeight + ImageWidth * ImageHeight / 4).Take(ImageWidth * ImageHeight).ToArray();
+            _y = _buffer.Skip(0).Take(ImageWidth * ImageHeight).ToArray();
+            _u = _buffer.Skip(ImageWidth * ImageHeight).Take(ImageWidth * ImageHeight / 4).ToArray();
+            _v = _buffer.Skip(ImageWidth * ImageHeight + ImageWidth * ImageHeight / 4).Take(ImageWidth * ImageHeight).ToArray();
+
+            for (int y = 0; y < ImageHeight; y++)
+            {
+                for (int x = 0; x < ImageHeight; x++)
+                {
+                    int i = x + y * ImageWidth;
+                    int j = (x >> 1) + (y >> 1) * (ImageWidth >> 1);
+
+                    var Y = _y[i] - 16;
+                    var U = _u[j] - 128;
+                    var V = _v[j] - 128;
+
+                    var R = 1.164F * Y + 1.596F * V;
+                    var G = 1.164F * Y - 0.813F * V - 0.391F * U;
+                    var B = 1.164F * Y + 2.018F * U;
+
+                    _r[i] = AsByte(R);
+                    _g[i] = AsByte(G);
+                    _b[i] = AsByte(B);
+
+                    //_r[i] = AsByte((298 * c + 409 * e + 128) >> 8);
+                    //_g[i] = AsByte((298 * c - 100 * d - 208 * e + 128) >> 8);
+                    //_b[i] = AsByte((298 * c + 516 * d + 128) >> 8);
+                }
+            }
 
             // https://docs.microsoft.com/en-us/previous-versions/aa917087(v=msdn.10)
-            for (int i = 0; i < (ImageWidth * ImageHeight); i++)
-            {
-                byte c = y[i];
-                byte d = u[i];
-                byte e = v[i];
+            //while (_counterY < ImageWidth * ImageHeight && _counterU < ImageWidth * ImageHeight / 4 && _counterV < ImageWidth * ImageHeight / 4)
+            //{
+            //    c = _y[_counterY];
 
-                r[i] = AsByte((298 * c + 409 * e + 128) >> 8);
-                g[i] = AsByte((298 * c - 100 * d - 208 * e + 128) >> 8);
-                b[i] = AsByte((298 * c + 516 * d + 128) >> 8);
+            //    if (_counterY % 3 == 0 || _isFirstCycle)
+            //    {
+            //        d = _u[_counterU];
+            //        e = _v[_counterV];
+            //    }
+
+            //    _r[_counterY] = AsByte((298 * c + 409 * e + 128) >> 8);
+            //    _g[_counterY] = AsByte((298 * c - 100 * d - 208 * e + 128) >> 8);
+            //    _b[_counterY] = AsByte((298 * c + 516 * d + 128) >> 8);
+
+            //    ++_counterY;
+            //}
+
+            bmp = new Bitmap(ImageWidth, ImageHeight, PixelFormat.Format24bppRgb);
+
+            for (int y = 0; y < ImageHeight; y++)
+            {
+                for (int x = 0; x < ImageHeight; x++)
+                {
+                    int i = x + y * ImageWidth;
+                    Color c = Color.FromArgb(_r[i], _g[i], _b[i]);
+                    bmp.SetPixel(x, y, c);
+                }
             }
+
+            Invalidate();
+
+            // TODO: Convert r, g, b arrays to bitmap
         }
 
-        private static byte AsByte(int value)
+        private static byte AsByte(float value)
         {
             //return (byte)value;
             if (value > 255)
+            {
                 return 255;
+            }
+
             if (value < 0)
+            {
                 return 0;
+            }
+
             return (byte)value;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bmp, 0, 0);
+            base.OnPaint(e);
         }
     }
 }
