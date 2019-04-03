b574c72358300fefdce90ed2026f520a744dd4f6
Peter Versywvelen <bugfact@gmail.com>
2019-04-03 15:51:10 +0200

    Faster bitmap update

--- ImageDisplayer/ImageView.cs
+++ ImageDisplayer/ImageView.cs
@@ -3,103 +3,89 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
 
 namespace ImageDisplayer
 {
     public partial class ImageView : Form
     {
-        private const int ImageWidth = 256;
-        private const int ImageHeight = 192;
-        private readonly int _counterY = 0;
-        private readonly int _counterU = 0;
-        private readonly int _counterV = 0;
-        private readonly bool _isFirstCycle = true;
+        private const int FullFrameWidth = 256;
+        private const int FullFrameHeight = 192;
+        private const int FullFrameSize = FullFrameWidth * FullFrameHeight;
 
-        private readonly byte[] _buffer = File.ReadAllBytes(Environment.CurrentDirectory + "/image.yuv");
+        private const int HalfFrameWidth = FullFrameWidth / 2;
+        private const int HalfFrameHeight = FullFrameHeight / 2;
+        private const int HalfFrameSize = HalfFrameWidth * HalfFrameHeight;
 
-        private readonly byte[] _y;
-        private readonly byte[] _u;
-        private readonly byte[] _v;
-        private readonly byte[] _r;
-        private readonly byte[] _g;
-        private readonly byte[] _b;
+        private Bitmap _rgbBitmap = new Bitmap(FullFrameWidth, FullFrameHeight, PixelFormat.Format24bppRgb);
 
-        private Bitmap bmp;
+        private MemoryStream _yuvStream;
+
+        private Timer _timer = new Timer();
+
+        private readonly byte[] _y = new byte[FullFrameSize];
+        private readonly byte[] _u = new byte[FullFrameSize];
+        private readonly byte[] _v = new byte[FullFrameSize];
+
 
         public ImageView()
         {
             InitializeComponent();
 
-            _r = new byte[ImageWidth * ImageHeight];
-            _g = new byte[ImageWidth * ImageHeight];
-            _b = new byte[ImageWidth * ImageHeight];
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.Opaque, true);
+            SetStyle(ControlStyles.DoubleBuffer|ControlStyles.OptimizedDoubleBuffer, false);
 
-            _y = _buffer.Skip(0).Take(ImageWidth * ImageHeight).ToArray();
-            _u = _buffer.Skip(ImageWidth * ImageHeight).Take(ImageWidth * ImageHeight / 4).ToArray();
-            _v = _buffer.Skip(ImageWidth * ImageHeight + ImageWidth * ImageHeight / 4).Take(ImageWidth * ImageHeight).ToArray();
+            _yuvStream = new MemoryStream(File.ReadAllBytes(Environment.CurrentDirectory + "/image.yuv"));
 
-            for (int y = 0; y < ImageHeight; y++)
-            {
-                for (int x = 0; x < ImageWidth; x++)
-                {
-                    int i = x + y * ImageWidth;
-                    int j = (x >> 1) + (y >> 1) * (ImageWidth >> 1);
-
-                    var Y = _y[i] - 16;
-                    var U = _u[j] - 128;
-                    var V = _v[j] - 128;
-
-                    var R = 1.164F * Y + 1.596F * V;
-                    var G = 1.164F * Y - 0.813F * V - 0.391F * U;
-                    var B = 1.164F * Y + 2.018F * U;
-
-                    _r[i] = AsByte(R);
-                    _g[i] = AsByte(G);
-                    _b[i] = AsByte(B);
-
-                    //_r[i] = AsByte((298 * c + 409 * e + 128) >> 8);
-                    //_g[i] = AsByte((298 * c - 100 * d - 208 * e + 128) >> 8);
-                    //_b[i] = AsByte((298 * c + 516 * d + 128) >> 8);
-                }
-            }
-
-            // https://docs.microsoft.com/en-us/previous-versions/aa917087(v=msdn.10)
-            //while (_counterY < ImageWidth * ImageHeight && _counterU < ImageWidth * ImageHeight / 4 && _counterV < ImageWidth * ImageHeight / 4)
-            //{
-            //    c = _y[_counterY];
-
-            //    if (_counterY % 3 == 0 || _isFirstCycle)
-            //    {
-            //        d = _u[_counterU];
-            //        e = _v[_counterV];
-            //    }
-
-            //    _r[_counterY] = AsByte((298 * c + 409 * e + 128) >> 8);
-            //    _g[_counterY] = AsByte((298 * c - 100 * d - 208 * e + 128) >> 8);
-            //    _b[_counterY] = AsByte((298 * c + 516 * d + 128) >> 8);
-
-            //    ++_counterY;
-            //}
-
-            bmp = new Bitmap(ImageWidth, ImageHeight, PixelFormat.Format24bppRgb);
-
-            for (int y = 0; y < ImageHeight; y++)
-            {
-                for (int x = 0; x < ImageWidth; x++)
-                {
-                    int i = x + y * ImageWidth;
-                    Color c = Color.FromArgb(_r[i], _g[i], _b[i]);
-                    bmp.SetPixel(x, y, c);
-                }
-            }
+            var displayer = new Action(DisplayNextFrame);
+            _timer.Interval = 1000/60;
+            _timer.Tick += delegate { Invoke(displayer); };
+            _timer.Start();
 
             Invalidate();
-
-            // TODO: Convert r, g, b arrays to bitmap
+            Update();
         }
 
-        private static byte AsByte(float value)
+        private unsafe void DisplayNextFrame()
+        {
+            if (_yuvStream.Read(_y, 0, FullFrameSize) <= 0 ||
+                _yuvStream.Read(_u, 0, HalfFrameSize) <= 0 ||
+                _yuvStream.Read(_v, 0, HalfFrameSize) <= 0)
+            {
+                _yuvStream.Position = 0;
+                return;
+            }
+
+            var data = _rgbBitmap.LockBits(new Rectangle(0, 0, FullFrameWidth, FullFrameHeight), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            var rgb = (byte*)data.Scan0.ToPointer();
+
+            for (int y = 0; y < FullFrameHeight; y++)
+            {
+                var yOffset = y * FullFrameWidth;
+                var cOffset = (y>>1) * HalfFrameWidth;
+
+                var rgbOffset = y * data.Stride;
+
+                for (int x = 0, t = 0; x < FullFrameWidth; x++, t += 3)
+                {
+                    var C = _y[yOffset + x] - 16;
+                    var D = _u[cOffset + (x >> 1)] - 128;
+                    var E = _v[cOffset + (x >> 1)] - 128;
+                    rgb[rgbOffset + t + 0] = AsByte((298 * C + 409 * E + 128) >> 8);
+                    rgb[rgbOffset + t + 1] = AsByte((298 * C - 100 * D - 208 * E + 128) >> 8);
+                    rgb[rgbOffset + t + 2] = AsByte((298 * C + 516 * D + 128) >> 8);
+                }
+            }
+
+            _rgbBitmap.UnlockBits(data);
+
+            Invalidate();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static byte AsByte(int value)
         {
             //return (byte)value;
             if (value > 255)
@@ -117,7 +103,13 @@ namespace ImageDisplayer
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            e.Graphics.DrawImage(bmp, 0, 0);
+            if (_yuvStream.Position == 0)
+            {
+                e.Graphics.Clear(Color.SkyBlue);
+            }
+
+            e.Graphics.DrawImage(_rgbBitmap, 0, 0);
+
             base.OnPaint(e);
         }
 
